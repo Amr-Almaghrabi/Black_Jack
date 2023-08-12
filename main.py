751175f1d511a9e5f0423bbe10977ff29f7f3a9a
Amr-Almaghrabi <106653139+Amr-Almaghrabi@users.noreply.github.com>
2023-08-12 15:07:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -33,13 +33,13 @@ def black_jack():
   
 
   
-  print(f"your current hand cards are: {user_hand} totaling {user_total}")
+  print(f"Your current hand cards are: {user_hand} totaling {user_total}")
   
   computer_hand =[]
   
   computer_hand.append(random.choice(cards))
   
-  print(f"the computer's hand is {computer_hand}")
+  print(f"The computer's hand is {computer_hand}")
   
   
   
@@ -52,34 +52,55 @@ def black_jack():
       user_total += new_card
       print(f"your cards are {user_hand}, total is {user_total}")
       more_cards = input("Would you like to draw another card ? (type y/n) ")
+
+    if user_total >21 and 11 in user_hand:
+      user_hand.remove(11)
+      user_hand.append(1)
+
+    if sum(user_hand) == 21 and len(user_hand) == 2:
+      print(f"You have blackjack , your cards are {user_hand} totaling 0")
+      return 0
+    
+    
+
+    user_total = sum(user_hand)
     
     new_card = random.choice(cards)
     computer_hand.append(new_card)
-    computer_total = 0
-    for i in computer_hand:
-      computer_total += i
+    computer_total = sum(computer_hand)
+
+    if computer_total >21 and 11 in computer_hand:
+      computer_hand.remove(11)
+      computer_hand.append(1)
+
+    computer_total = sum(computer_hand)
+    print(f"Your hand is {user_hand}")
+    print(f"Computer hand is {computer_hand}")
+
     
     if user_total > 21:
       print( 'You have a bust, you lose :(') 
   
     elif computer_total >21:
-      print(f"Computer has a bust, cards are {computer_hand}, you win!")
+      print(f"Computer has a bust, you win!")
     
     elif user_total == computer_total:
       print(f"it's a draw")
   
     elif user_total > computer_total:
-      print(f"Your total is {user_total} and computer cards are {computer_hand}, you win !")
+      print(f"Your total is {user_total} and computer total is {computer_total}, you win !")
   
     else:
-      print(f"computer cards are {computer_hand}, computer wins :( ")
+      print(f"Your total is {user_total}, computer total is {computer_total} computer wins :( ")
   
     play_again = input("Would you like to play again? (yes/no) ")
     play_again = play_again.lower()
+    
     if play_again == 'yes':
       replit.clear()
       black_jack()
     else:
+      checker = False
       return
 
   return
